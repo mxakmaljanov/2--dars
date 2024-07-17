458c61895f0ad9e44cb45fca004eda8e39df29de
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-07-17 22:11:12 +0500

    2--dars

---
+++ index.js
@@ -0,0 +1,98 @@
+//1misol
+//let counter=0
+//let son= +prompt("sonni kiriting")
+//let raqam;
+//if(raqam % 2 == 0){
+  //  raqam = raqam + 1
+//}
+//2-misol
+//let counter = 0
+//let son= +prompt("sonni kiriting")
+//let raqam;
+//if(raqam % 2 == 0){
+  //  raqam = raqam+1
+//}
+//else{
+  //  raqam = raqam-2
+//}
+//3-misol
+//let counter = 0
+//let son1 = +prompt("1-sonni kiriting")
+//let son1 = +prompt("1-sonni kiriting")
+//let max;
+//if(son1 > son2){
+  //  max = son1
+//}else{
+  //  max = son2
+//}
+//console.log(max)
+
+//4-misol
+//let counter=0
+//let son1 = +prompt("1-sonni kiriting")
+//let son2 = +prompt("2-sonni kiriting")
+//let max;
+//let min;
+//if(son1 > son2){
+//    max = son1
+//}
+//else{
+//    max = son2
+//}
+//if(son1 < son2){
+//    min = son1
+//}else{
+//    min = son1
+//}
+//console.log(max)
+//console.log(min)
+//5-misol
+//let counter=0
+//let num1 = +prompt("1-sonni kiri ting")
+//let num2 = +prompt("2-sonni kiri ting")
+//let num3 = +prompt("3-sonni kiri ting")
+//let max;
+//if(num1 = num2 + num3){
+//max = num1
+//}
+//else{
+//  max = num2
+//}else{
+//  max = num3
+//}6-misol
+//let counter =0
+//let son1 = +prompt("1-sonni kiriting")
+//let son2 = +prompt("2-sonni kiriting")
+//let son3 = +prompt("3-sonni kiriting")
+//let max;
+//let min;
+//if(son1 > son2){
+//    max = son1
+//}
+//else{
+//    max = son2
+//}
+//if(son1 < son2){
+//    min = son1
+//}else{
+//    min = son1
+//}
+//if(son2 < son3){
+//  max = son3
+//}
+//else{
+//  min = son3
+//}
+//console.log(max)
+//console.log(min)
+//7-misol
+//let counter = 0
+//let son1 = +prompt("1-sonni kiriting")
+//let son2 = +prompt("2-sonni kiriting")
+//let son3 = + prompt("3-sonni kiriting")
+//
+//let ortacha;
+//
+//if (son1 + son2+son3){ 
+//  ortacha = son1 + son2 + son3
+//}
